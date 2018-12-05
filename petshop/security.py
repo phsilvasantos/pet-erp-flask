7def762c146a52b838d2ae7ab9e22a09932769b1
Erick Gomes <Erick.Gomes@controlrisks.com>
2018-12-05 11:20:11 -0500

    removed crud

--- petshop/security.py
+++
@@ -1,9 +0,0 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager
-
-import credenciais
-
-passw = credenciais.mypass
-hpass = generate_password_hash(passw)
-
-check = check_password_hash(hpass, credenciais.mypass)
